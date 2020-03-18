3cc29768762fb7f344a998544d0de8a9fdd90f3b
Chris Eadie <chris.eadie@outlook.com>
2020-03-18 13:27:53 +0000

    Add comments creation

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,41 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.create(comment_params)
+    redirect_to posts_url
+  end
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+    redirect_to posts_path
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+    redirect_to posts_path, :notice => "Your comment has been updated"
+      else 
+        render "edit"
+      end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to posts_path
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :post_id)
+  end
+end
