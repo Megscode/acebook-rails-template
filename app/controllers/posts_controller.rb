648e99b093d04d3661c6f3fcd3141457b1c24767
Chris Eadie <chris.eadie@outlook.com>
2020-03-19 17:42:28 +0000

    Lower PostController complexity

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,19 +20,9 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.user_id == current_user.id
-      if @post.created_at > 10.minutes.ago
-        if @post.update(post_params)
-          redirect_to posts_path, :notice => "Your post has been updated"
-        else
-          render "edit"
-        end
-      else
-        flash[:alert] = "Posts can only be updated within 10 minutes of creation!"
-        redirect_to @post
-      end
+      time_limit_check
     else
-      flash[:alert] = "Posts can only be updated by the user who created them!"
-      redirect_to @post
+      deny_non_user
     end
   end
 
@@ -56,5 +46,31 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:message, :user_id)
+  end
+
+  def deny_non_user
+    flash[:alert] = "Posts can only be updated by the user who created them!"
+    redirect_to @post
+  end
+
+  def post_or_rerender
+    if @post.update(post_params)
+      redirect_to posts_path, :notice => "Your post has been updated"
+    else
+      render "edit"
+    end
+  end
+
+  def too_late
+    flash[:alert] = "Posts can only be updated within 10 minutes of creation!"
+    redirect_to @post
+  end
+
+  def time_limit_check
+    if @post.created_at > 10.minutes.ago
+      post_or_rerender
+    else
+      too_late
+    end
   end
 end
