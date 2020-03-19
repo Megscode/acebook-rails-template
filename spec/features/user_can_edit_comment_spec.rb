ac48dfa6e98cf638d5054bc0fc77028b10792207
Megan Hayes <student@makerss-air.home>
2020-03-19 16:33:19 +0000

    editing function in progress

--- spec/features/user_can_edit_comment_spec.rb
+++ spec/features/user_can_edit_comment_spec.rb
@@ -14,6 +14,24 @@ RSpec.feature "posts list with comments", type: :feature do
     click_button "Submit"
     visit "/posts"
     expect(page.all("#posts").first.text).to include("Update comment")
+  end
+  scenario "User can edit their comment" do
+    user_sign_up
+
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+
+    click_link "Add comment"
+    fill_in "Content", with: "Comment on post"
+    click_button "Submit"
+
+    # visit "/posts"
+    click_link "Update comment"
+    fill_in "Content", with: "Bye, sheep!"
+    click_button "Update comment"
     
+    expect(page.all("#posts").first.text).to include("Bye, sheep!")
   end
 end
