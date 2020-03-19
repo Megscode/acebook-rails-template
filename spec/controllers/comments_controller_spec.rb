024543288e11753f7e429793bc7569c6e4d0b7a9
Chris Eadie <chris.eadie@outlook.com>
2020-03-19 11:29:11 +0000

    Add unit test for CommentsController

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,24 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-  # before(:each) do
-  #   @user = User.create!({
-  #     :email => "test@test.com",
-  #     :password => "password",
-  #   })
-  #   sign_in @user
-  # end
+  before(:each) do
+    @user = User.create!({
+      :email => "test@test.com",
+      :password => "password",
+    })
 
-  # describe "POST /" do
-  #   it "responds with 200" do
-  #     comment :create, params: { post: { message: "Hello, world!" } }
-  #     :content, :user_id, :post_id
-  #     expect(response).to redirect_to(posts_url)
-  #   end
+    sign_in @user
+  end
+
+  describe "POST /" do
+    it "responds with 200" do
+      post :create, params: { comment: { content: "Commenty thing", user_id: "1", post_id: "1"}}
+      
+      expect(response).to redirect_to(posts_url)
+    end
     
-  #   it "creates a post and re routes user" do
-  #     post :create, params: { post: { message: "Hello, world!" } }
-  #     expect(response).to have_http_status(302)
-  #   end
-  # end
+    it "creates a comment and re routes user" do
+      post :create, params: { comment: { content: "Commenty thing", user_id: "1", post_id: "1"}}
+      
+      expect(response).to have_http_status(302)
+    end
+  end
 end
