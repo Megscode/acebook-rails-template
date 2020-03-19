7c9c874760cdb6f9a0a3a98a034797c63c85c164
Chris Eadie <chris.eadie@outlook.com>
2020-03-19 17:49:49 +0000

    Updated missing test to fix example email address

--- spec/features/user_can_add_comment_to_post_spec.rb
+++ spec/features/user_can_add_comment_to_post_spec.rb
@@ -16,4 +16,22 @@ RSpec.feature "Timeline", type: :feature do
 
     expect(page).to have_content("Comment on post")
   end
+
+  scenario "Can see who commented and when" do
+    user_sign_up
+    travel_to Time.zone.local(2020, 3, 12, 00, 00, 00)
+
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+
+    click_link "Add comment"
+    fill_in "Content", with: "Comment on post"
+    click_button "Submit"
+    visit "/posts"
+
+    expect(page).to have_content("by example@example.com")
+    expect(page).to have_content("2020-03-12 00:00:00 UTC")
+  end
 end
